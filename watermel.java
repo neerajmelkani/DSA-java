2bdb154b43c0ff3fe13b53118e62b24c7ede31a6
neeraj melkani <melkanineeraj86@gmail.com>
2026-04-01 23:54:35 +0530

    Add files via upload

---
+++ watermel.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class watermel {
+    public static void main(String[] args) {
+     
+        Scanner sc = new Scanner(System.in);
+        int weight = sc.nextInt();
+        if(weight%2==0 && weight!=2){
+            System.out.println("YES");
+         } else{
+            System.out.println("NO");
+         }
+        }
+    }
+    
+
