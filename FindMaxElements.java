a1ca8c446736b5218ec8d0be8cf148aa5b99d08a
neeraj melkani <melkanineeraj86@gmail.com>
2026-04-11 09:07:38 +0530

    Add files via upload

---
+++ FindMaxElements.java
@@ -0,0 +1,20 @@
+public class FindMaxElements {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 1, 2, 4, 8};
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Minimum: " + min);
+        System.out.println("Maximum: " + max);
+    }
+}  
