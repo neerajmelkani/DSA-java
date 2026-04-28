bbcb6eb1cd8221ab459f6fa8d2886b661b80443c
neeraj melkani <melkanineeraj86@gmail.com>
2026-04-28 17:46:31 +0530

    Add files via upload

---
+++ MaxSubrray.java
@@ -0,0 +1,16 @@
+class MaxSubrray {
+    public int maxSub(int[] nums) {
+        int maxSum = nums[0];
+        int currSum = 0;
+
+        for (int num : nums) {
+            currSum += num;
+            maxSum = Math.max(maxSum, currSum);
+            
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
