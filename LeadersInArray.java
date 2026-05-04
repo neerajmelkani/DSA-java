618b968762ef22e347c8236f2c9e821aca47a217
neeraj melkani <melkanineeraj86@gmail.com>
2026-05-04 20:15:43 +0530

    Add files via upload

---
+++ LeadersInArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class LeadersInArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        int maxRight = arr[n - 1];
+        System.out.print(maxRight + " ");
+
+        for(int i = n - 2; i >= 0; i--) {
+            if(arr[i] > maxRight) {
+                System.out.print(arr[i] + " ");
+                maxRight = arr[i];
+            }
+        }
+    }
+}
+
