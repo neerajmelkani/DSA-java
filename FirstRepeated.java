932b819f1e2f15f0f69047ca51d3d5595e190aa1
neeraj melkani <melkanineeraj86@gmail.com>
2026-05-07 12:43:58 +0530

    Add files via upload

---
+++ FirstRepeated.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class FirstRepeated {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        HashSet<Character> set = new HashSet<>();
+
+        for(char c : str.toCharArray()) {
+            if(set.contains(c)) {
+                System.out.println("First Repeated: " + c);
+                return;
+            }
+            set.add(c);
+        }
+
+        System.out.println("No Repeated Character");
+    }
+}
