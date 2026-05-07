95833c015f7a164ae6e4aa60654f2d125cf458b0
neeraj melkani <melkanineeraj86@gmail.com>
2026-05-07 12:45:53 +0530

    Add files via upload

---
+++ RearrangeNums.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class RearrangeNums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int left = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] < 0) {
+                int temp = arr[i];
+                arr[i] = arr[left];
+                arr[left] = temp;
+                left++;
+            }
+        }
+
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
