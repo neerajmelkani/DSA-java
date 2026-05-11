fa27c289d0887a4547f40ff6cb2f9cf6a6cfc29d
neeraj melkani <melkanineeraj86@gmail.com>
2026-05-11 23:53:56 +0530

    Add files via upload

---
+++ SumOfEvenIndex.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class SumOfEvenIndex {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+
+        for(int i = 0; i < n; i += 2) {
+            sum += arr[i];
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
