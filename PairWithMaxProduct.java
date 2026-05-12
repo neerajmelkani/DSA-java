a9eba69a20483609511a6b6cba5a8f803925d8c1
neeraj melkani <melkanineeraj86@gmail.com>
2026-05-12 20:50:49 +0530

    Add files via upload

---
+++ PairWithMaxProduct.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class PairWithMaxProduct {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for(int num : arr) {
+
+            if(num > first) {
+                second = first;
+                first = num;
+            }
+            else if(num > second) {
+                second = num;
+            }
+        }
+
+        System.out.println("Pair: " + first + " " + second);
+    }
+}
