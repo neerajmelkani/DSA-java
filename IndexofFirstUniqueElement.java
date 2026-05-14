5b180db9e47603d7a06e712cf348f006903f4186
neeraj melkani <melkanineeraj86@gmail.com>
2026-05-14 21:59:19 +0530

    Add files via upload

---
+++ IndexofFirstUniqueElement.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class IndexofFirstUniqueElement {
+
+    import java.util.*;
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+
+            for(int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            HashMap<Integer, Integer> map = new HashMap<>();
+
+            for(int num : arr) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+
+            for(int i = 0; i < n; i++) {
+
+                if(map.get(arr[i]) == 1) {
+                    System.out.println("Index: " + i);
+                    return;
+                }
+            }
+
+            System.out.println("No Unique Element");
+        }
+    }
