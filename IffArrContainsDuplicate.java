de4cf13ac1fe486937e08ad0958b188aae3ae79d
neeraj melkani <melkanineeraj86@gmail.com>
2026-05-16 16:07:57 +0530

    Add files via upload

---
+++ IffArrContainsDuplicate.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class IffArrContainsDuplicate {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+
+            HashSet<Integer> set = new HashSet<>();
+
+            boolean found = false;
+
+            for(int i = 0; i < n; i++) {
+
+                arr[i] = sc.nextInt();
+
+                if(set.contains(arr[i])) {
+                    found = true;
+                }
+
+                set.add(arr[i]);
+            }
+
+            if(found) {
+                System.out.println("Duplicate Found");
+            } else {
+                System.out.println("No Duplicate");
+            }
+        }
+    }
+
