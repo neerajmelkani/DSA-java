7713d4a184d2753ad4e62a7f49059e66a70487b4
neeraj melkani <melkanineeraj86@gmail.com>
2026-05-20 22:31:45 +0530

    Add files via upload

---
+++ RotateArrLeftBy1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class RotateArrLeftBy1 {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+
+            for(int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int first = arr[0];
+
+            for(int i = 0; i < n - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+
+            arr[n - 1] = first;
+
+            for(int num : arr) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
