471cca809d655f8d2583fbfe96e4197ecb8201bb
neeraj melkani <melkanineeraj86@gmail.com>
2026-05-25 17:52:17 +0530

    Add files via upload

---
+++ MaximumDifference.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class MaximumDifference {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+
+            for(int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int maxDiff = Integer.MIN_VALUE;
+
+            for(int i = 0; i < n; i++) {
+
+                for(int j = i + 1; j < n; j++) {
+
+                    maxDiff = Math.max(maxDiff, Math.abs(arr[i] - arr[j]));
+                }
+            }
+
+            System.out.println(maxDiff);
+        }
+
+}
