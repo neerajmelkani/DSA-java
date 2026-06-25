c18a1328494e967faa88961d9eb550d07ef52daa
neeraj melkani <melkanineeraj86@gmail.com>
2026-06-25 21:36:12 +0530

    Refactor LeftRotate

--- LeftRotate.java
+++ LeftRotate.java
@@ -16,14 +16,11 @@ import java.util.*;
         System.out.println(Arrays.toString(arr));
     }
 }*/
-
 public class LeftRotate {
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-
-
     for(int i = 0; i<n; i++){
         arr[i] = sc.nextInt();
     }
