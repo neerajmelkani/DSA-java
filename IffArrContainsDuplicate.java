474aca39936ab1282107a63f3acdc66249637a80
neeraj melkani <melkanineeraj86@gmail.com>
2026-06-29 21:42:04 +0530

    Update IffArrContainsDuplicate.java

--- IffArrContainsDuplicate.java
+++ IffArrContainsDuplicate.java
@@ -1,8 +1,6 @@
 import java.util.*;
 public class IffArrContainsDuplicate {
-
         public static void main(String[] args) {
-
             Scanner sc = new Scanner(System.in);
 
             int n = sc.nextInt();
